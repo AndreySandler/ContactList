be56efd761533d9557e75ed02f98370376921c4d
AndreySandler <mrdreem13@gmail.com>
2022-10-18 18:06:59 +0300

    Add model, service (MVC)

---
+++ ContactList/Models/Person.swift
@@ -0,0 +1,13 @@
+//
+//  Person.swift
+//  ContactList
+//
+//  Created by Andrey Sandler on 18.10.2022.
+//
+
+struct Person {
+    let name: String
+    let surname: String
+    let email: String
+    let phoneNumber: String
+}
---
+++ ContactList/Services/dataManager.swift
@@ -0,0 +1,41 @@
+//
+//  dataManager.swift
+//  ContactList
+//
+//  Created by Andrey Sandler on 18.10.2022.
+//
+
+class DataManager {
+    let names = [
+        "Hasanshik",
+        "Andrey",
+        "Poor",
+        "Alexey",
+        "Slavik",
+        "Nikita"
+    ]
+    let surnames = [
+        "Dikii",
+        "Developer",
+        "Relative",
+        "Efimov",
+        "Talker",
+        "Phycho"
+    ]
+    let emails = [
+        "hasan4ik@yandex.ru",
+        "xcodeKing@yandex.ru",
+        "vad1m@yandex.ru",
+        "alexey.efimov@yandex.ru",
+        "talker@yandex.ru",
+        "phycho@gmail.com"
+    ]
+    let phoneNumbers = [
+        "89486018395",
+        "89637503859",
+        "89324628506",
+        "89840683819",
+        "89205748392",
+        "89775840698"
+    ]
+}
